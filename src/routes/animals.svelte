2813a2071ef92b6535d8edbeae45f8cd950216a3
indyandie <1058837+Indyandie@users.noreply.github.com>
2022-07-10 15:08:47 -0500

    init

---
+++ src/routes/animals.svelte
@@ -0,0 +1,28 @@
+<script>
+  console.log('hello world')
+  const animals = [
+    {name: "dog", emoji: "🐕🐶"},
+    {name: "lion", emoji: "🦁"},
+    {name: "monkey", emoji: "🐒🐵"},
+    {name: "pig", emoji: "🐖🐷"},
+    {name: "camel", emoji: "🐫"},
+    {name: "cat", emoji: "🐈🐱"}
+  ]
+</script>
+
+<main>
+  <h1>Animals</h1>
+
+  {#each animals as animal}
+  <div class="animals">
+    {animal.emoji}
+    <div>{animal.name}</div>
+  </div>
+  {/each}
+</main>
+
+<style>
+  .animals {
+    font-size: 78px;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+
+<main>
+  <h1>Home</h1>
+  <p>
+    Getting started with SvelteKit.
+  </p>
+</main>
